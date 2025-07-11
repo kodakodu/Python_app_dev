19d0d57666aa9ac31b7de163872ce32c2ec4702e
kodakodu <kodakodu@gmail.com>
2025-07-11 15:06:45 +0200

    53:05

--- main.py
+++ main.py
@@ -8,6 +8,20 @@ main_window.setWindowTitle("Random Word Maker")
 main_window.resize(300,200)
 
 #Create all Object/Widget below here
+title = QLabel("Random Keywords")
+
+text1 = QLabel("?")
+text2 = QLabel("?")
+text3 = QLabel("?")
+
+button1 =QPushButton("CLick Me")
+button2 =QPushButton("CLick Me")
+button3 =QPushButton("CLick Me")
+
+#All design here
+master_layout = QVBoxLayout()
+
+
 
 main_window.show()
 app.exec()
